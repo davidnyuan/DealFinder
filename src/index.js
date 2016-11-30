5e92c4f28e5af8f640c34549fe05c87818ce3437
ah46657 <ah46657@uw.edu>
2016-11-30 08:50:38 -0800

    added basic skeleton code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import firebase from 'firebase';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router'
+
+//load our CSS files
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
