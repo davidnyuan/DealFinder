982954b557a017fcd1e515a46813d547083bb719
David Yuan <davidnyuan@gmail.com>
2016-12-01 16:52:02 -0800

    Created dealObject object to help keep track of individual deals

---
+++ src/dealObject.js
@@ -0,0 +1,15 @@
+class dealObject {
+
+    constructor(iName, cName, cPrice, dPrice, oPrice, iURL, wURL, sCompany) {
+        this.itemName = iName;
+        this.companyName = cName;
+        this.currentPrice = cPrice;
+        this.discountRate = dPrice;
+        this.originalPrice = oPrice;
+        this.imageURL = iURL;
+        this.websiteURL = wURL;
+        this.sellerCompany = sCompany;
+    }
+}
+
+export default dealObject;
