30d157edecb8f0f466d069aaaaf1edbb2e89adab
Tyler Foster <TyFoster@UW.edu>
2016-12-03 18:36:29 -0800

    Added collapsible filter to search

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import DataController from './DataController.js';
 import dealObject from './dealObject.js';
+import {Button, Collapse} from 'react-bootstrap';
 
 class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
       <div>
-        <p>Search Page</p>
-        <ItemObject itemName="Thing" companyName="company" currentPrice="$40.00" discount="40%"
+        <form id="searchForm">
+          <input type="text" /><input type="submit" />
+        </form>
+
+        <div id="filterOptions">
+        <Button id="filterButton" onClick={ ()=> this.setState({ open: !this.state.open })}>
+          Filter <span className="caret"></span>
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+              Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </Collapse>
+        </div>
+
+        <div id="searchResults">
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
                     imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        <ItemObject itemName="Google" companyName="company" currentPrice="$40.00" discount="40%"
+                    imageUrl="defaultImg.png" websiteUrl="https://www.google.com/" sourceName="Sqoot" />
+        </div>
       </div>
     );
   }
@@ -25,7 +59,7 @@ class ItemObject extends React.Component {
         <p className="itemInfo">
           <span className="itemName">{this.props.itemName}</span> <br />
           <span className="itemPrice">{this.props.currentPrice} </span> 
-          {this.props.discount} <br />
+          <span className="itemDiscount">{this.props.discount} off </span><br />
           Found via {this.props.sourceName}
         </p>
       </div>
