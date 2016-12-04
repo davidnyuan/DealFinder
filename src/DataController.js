e20318859211093c6f84c610ded81ddcf6fbab49
David Yuan <davidnyuan@gmail.com>
2016-12-04 13:06:03 -0800

    Finished fetch call for sqoot

--- src/DataController.js
+++ src/DataController.js
@@ -1,12 +1,26 @@
+import dealObject from './dealObject.js';
+import jQuery from '//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js';
+
+var objectArray = [];
+
 class DataController {
     grabData(query) {
-        fetch('http://api.sqoot.com/v2/deals?api_key=nBk_SmX1WbhznkZ44N96&online=true&query=headphones')
+        query = query.replace(" ", "%20");
+        //sqoot
+        $.getJSON('http://api.sqoot.com/v2/deals?api_key=nBk_SmX1WbhznkZ44N96&online=true&query=' + query)
         .then((res) => {
-            return res.json();
+            for (var i in res.deals) {
+                if('key' in i === 'deal') {
+                    objectArray.append(dealObject(deal.title, deal.provider_name, deal.price, deal.discount_percentage, deal.value, deal.image_url, deal.url, deal.merchant.name.split(" ")[0]));
+                }
+            };
         })
-        .then(() => {
+        //amazon
+        // fetch('http://webservices.amazon.com/onca/xml?AWSAccessKeyId=AKIAJH52CXHDZPFFKDAA&AssociateTag=de032a-20&Keywords=' + query + '&Operation=ItemSearch&ResponseGroup=Offers&SearchIndex=All&Service=AWSECommerceService&Timestamp=2016-12-04T01%3A46%3A14.000Z&Signature=QNbC91Vmgl6sNIpQr71Stt%2B6JCHzkFCYf4N3Czsql%2FY%3D')
+        // .then((res) => { //parse through xml file
+        // })
 
-        })
+        return objectArray;
     }
 }
 
