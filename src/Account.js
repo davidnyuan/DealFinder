d74abc05329f566f4671fd23ac20bdeba704c67a
ah46657 <ah46657@uw.edu>
2016-12-03 17:13:27 -0800

    added basic redirection

--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import firebase from 'firebase';
+import {hashHistory} from 'react-router';
 
 class AccountPage extends React.Component {
+  componentDidMount() {
+    this.unregister = firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(!firebaseUser) {
+        if(this.unregister) {
+          this.unregister();
+        }
+        hashHistory.push('/login');
+      }
+    });
+  }
+
   render() {
     return (
       <div>
